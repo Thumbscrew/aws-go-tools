31be9b35e739e948fa3d84e068f2707905a3b99a
Thumbscrew <thumbscrw@pm.me>
2024-06-15 12:42:04 +0100

    add multithreaded downloader

---
+++ multimanager/downloader.go
@@ -0,0 +1,73 @@
+package multimanager
+
+import (
+	"context"
+	"io"
+	"sync"
+
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type DownloaderClient interface {
+	GetObject(context.Context, *s3.GetObjectInput, ...func(*s3.Options)) (*s3.GetObjectOutput, error)
+}
+
+type GetObjectsOutput struct {
+	Output int64
+	Err    error
+}
+
+type GetObjectsInput struct {
+	io.WriterAt
+	*s3.GetObjectInput
+}
+
+func GetObjects(ctx context.Context, c DownloaderClient, inputs []GetObjectsInput, o ...func(*manager.Downloader)) []GetObjectsOutput {
+	inputCount := len(inputs)
+	outputs := make([]GetObjectsOutput, inputCount)
+	outputChan := make(chan *GetObjectsOutput, inputCount)
+	defer close(outputChan)
+
+	wg := &sync.WaitGroup{}
+	for _, input := range inputs {
+		wg.Add(1)
+
+		go download(ctx, &downloadInput{
+			Client:     c,
+			WriterAt:   input.WriterAt,
+			Input:      input.GetObjectInput,
+			WaitGroup:  wg,
+			OutputChan: outputChan,
+		})
+	}
+
+	wg.Wait()
+
+	for i := 0; i < inputCount; i++ {
+		outputs[i] = *<-outputChan
+	}
+
+	close(outputChan)
+
+	return outputs
+}
+
+type downloadInput struct {
+	Client DownloaderClient
+	io.WriterAt
+	Input *s3.GetObjectInput
+	*sync.WaitGroup
+	OutputChan chan *GetObjectsOutput
+}
+
+func download(ctx context.Context, i *downloadInput, o ...func(*manager.Downloader)) {
+	defer i.WaitGroup.Done()
+
+	d := manager.NewDownloader(i.Client, o...)
+	n, err := d.Download(ctx, i.WriterAt, i.Input, o...)
+	i.OutputChan <- &GetObjectsOutput{
+		Output: n,
+		Err:    err,
+	}
+}
